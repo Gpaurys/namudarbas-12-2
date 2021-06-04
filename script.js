2202a31db63eecb081a36d983e025aab5c1d9035
Giedrius <paurys79@gmail.com>
2021-06-04 14:16:48 +0300

    Įkėliau antrą užduotį

---
+++ script.js
@@ -0,0 +1,13 @@
+alert("Naujas dalyvis: " + name);
+
+var names = ["JonSnow", "Cersei Lannister", "Daenerys Targaryen",
+"Theon Greyjoy", "Tyrion Lannister", "Arya Stark"];
+console.log(names[0] + ", " + names[5] + ". ");
+
+var name = prompt("Koks tavo vardas?");
+alert("Naujas dalyvis: " + name);
+
+var names = ["JonSnow", "Cersei Lannister", "Daenerys Targaryen",
+"Theon Greyjoy", "Tyrion Lannister", "Arya Stark"];
+var number = prompt("Koks tai skaičius?");
+console.log("Dalyvio vardas: " + names[number] + ". ");
